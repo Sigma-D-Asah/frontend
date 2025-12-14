3b51a6caf8c2526bf0bef76935e52233bea7f55c
Raihan Putra Kirana <me@raihanpk.com>
2025-12-14 15:05:22 +0700

    fix: update link to the correct deployment URL

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -18,7 +18,7 @@ export default function WelcomePage() {
               <li>
                 <a
                   className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                  href="https://sigma-asah.vercel.app"
+                  href="https://sigma-asah.netlify.app"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
